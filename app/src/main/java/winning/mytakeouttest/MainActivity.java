8b40a20c5d23312afbeab28c0bdf3023e4b190db
jtq@1036012350 <JTQc5235>
2017-01-17 13:24:19 +0800

    完成了底部栏的点击事件

--- app/src/main/java/winning/mytakeouttest/MainActivity.java
+++ app/src/main/java/winning/mytakeouttest/MainActivity.java
@@ -4,10 +4,10 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import winning.mytakeouttest.databinding.ActivityMainBinding;
-import winning.mytakeouttest.utils.ToastUtil;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding = null;
@@ -37,8 +37,32 @@ public class MainActivity extends AppCompatActivity {
 
     //通过点击的时候来改变UI
     private void chageUI(int index) {
-        ToastUtil.showText(MainActivity.this, index + "");
+//        ToastUtil.showText(MainActivity.this, index + "");
+        int childCount = binding.mainBottomeSwitcherContainer.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            if (i == index) {
+                setEnable(binding.mainBottomeSwitcherContainer.getChildAt(i), false);
+            } else {
+                setEnable(binding.mainBottomeSwitcherContainer.getChildAt(i), true);
+            }
+        }
+    }
 
+    /**
+     * 将每个Item中的所用控件状态一同改变
+     * 由于我们处理一个通用的代码，那么Item可能会有很多层，所以我们需要使用递归
+     *
+     * @param item
+     * @param b
+     */
+    private void setEnable(View item, boolean b) {
+        item.setEnabled(b);
+        if (item instanceof ViewGroup) {
+            int childCount = ((ViewGroup) item).getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                setEnable(((ViewGroup) item).getChildAt(i), b);
+            }
+        }
     }
 
 
