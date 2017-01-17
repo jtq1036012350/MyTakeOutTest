59748795c872014c852ff9d991a88ee2365e64df
jtq@1036012350 <JTQc5235>
2017-01-17 13:43:05 +0800

    完成了页面的切换逻辑

--- app/src/main/java/winning/mytakeouttest/MainActivity.java
+++ app/src/main/java/winning/mytakeouttest/MainActivity.java
@@ -2,22 +2,48 @@ package winning.mytakeouttest;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import java.util.ArrayList;
+
 import winning.mytakeouttest.databinding.ActivityMainBinding;
+import winning.mytakeouttest.fragments.HomeFragment;
+import winning.mytakeouttest.fragments.MeFragment;
+import winning.mytakeouttest.fragments.MoreFragment;
+import winning.mytakeouttest.fragments.OrderFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding = null;
+
+    //用来存放主界面的Fragemnt
+    private ArrayList<Fragment> fragments = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        //初始化数据
+        initDatas();
+        //初始化布局
         initViews();
+    }
+
+    private void initDatas() {
+        fragments = new ArrayList<>();
+        //添加Fragnment
+        fragments.add(new HomeFragment());
+        fragments.add(new OrderFragment());
+        fragments.add(new MeFragment());
+        fragments.add(new MoreFragment());
+
+        changeFragments(0);
+        //默认选择第一个条目
+        setEnable(binding.mainBottomeSwitcherContainer.getChildAt(0), false);
     }
 
     //初始化布局
@@ -29,14 +55,27 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     int index = binding.mainBottomeSwitcherContainer.indexOfChild(view);
-                    chageUI(index);
+                    changeUI(index);
+                    changeFragments(index);
                 }
+
+
             });
         }
     }
 
+    //切换Fragment
+    private void changeFragments(int index) {
+        Fragment fragment = fragments.get(index);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, fragment)
+                .commit();
+    }
+
     //通过点击的时候来改变UI
-    private void chageUI(int index) {
+    private void changeUI(int index) {
 //        ToastUtil.showText(MainActivity.this, index + "");
         int childCount = binding.mainBottomeSwitcherContainer.getChildCount();
         for (int i = 0; i < childCount; i++) {
